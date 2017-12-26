d01d564274bd8403064285e7d883afc2299c4597
Freddy Fallon <freddyfallon@gmail.com>
2017-12-26 12:48:42 +0000

    🎨 Add eslint-disable-next-line to reducers

--- src/reducers/reducer_authorities.js
+++ src/reducers/reducer_authorities.js
@@ -1,6 +1,7 @@
-import { FETCH_AUTHORITIES } from '../actions/fetchAuthorities'
+import { FETCH_AUTHORITIES } from '../actions/fetchAuthorities';
 
 export default function(state = [], action) {
+  // eslint-disable-next-line
   switch(action.type) {
     case FETCH_AUTHORITIES:
       return action.payload.data.authorities ;
--- src/reducers/reducer_ratings.js
+++ src/reducers/reducer_ratings.js
@@ -1,6 +1,7 @@
 import { FETCH_RATINGS } from '../actions/fetch_ratings';
 
 export default function(state = null, action) {
+  // eslint-disable-next-line
   switch(action.type) {
     case FETCH_RATINGS:
       return action.payload ;
